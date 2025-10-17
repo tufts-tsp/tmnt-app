51c7ae8fe83bba071105c71458d6a321d81c7044
hijodecoche <carsonpowers@yahoo.com>
2025-10-17 14:28:41 -0400

    Removed testing buttons, updated settings.py to reflect production environment, and updated Dockerfile so this will hopefully run in containerized.

--- app/tmnt/settings.py
+++ app/tmnt/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
     "django-insecure-1)y8msn7$=4c#8924c@pj1g+1_g(s&y&bo1=9l#v6h))%p@=ug"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["tmnt.cs.tufts.edu", "0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
