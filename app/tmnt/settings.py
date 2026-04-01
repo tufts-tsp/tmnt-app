19476928e8ac652d5f010eeccf117b51fab9ed64
hijodecoche <carsonpowers@yahoo.com>
2026-04-01 12:49:11 -0400

    Attempting to use new db

--- app/tmnt/settings.py
+++ app/tmnt/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = "tmnt.wsgi.application"
 
 DATABASES = {
     # REPLACE WITH MYSQL FOR PRODUCTION
-    # "default": {
-    #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": os.environ.get("DB_TITLE", "tmnt"),
-    #     'USER': os.environ.get("DB_USER", "tmnt"),
-    #     'PASSWORD': os.environ.get("DB_PASSWORD", ""),
-    #     'HOST': os.environ.get("DB_HOST", ""),
-    #     # "OPTIONS": {
-    #     #     "read_default_file": "/path/to/my.cnf",
-    #     # },
-    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_TITLE", "tmnt"),
+        'USER': os.environ.get("DB_USER", "tmnt"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", ""),
+        'HOST': os.environ.get("DB_HOST", ""),
+        # "OPTIONS": {
+        #     "read_default_file": "/path/to/my.cnf",
+        # },
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
