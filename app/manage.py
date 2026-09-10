2d0d1807deda1ae9011ddfefce99f6ea1255b8e8
hijodecoche <carsonpowers@yahoo.com>
2026-09-10 14:45:03 -0400

    Messed up main.

--- app/manage.py
+++ app/manage.py
@@ -6,10 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    if sys.argv[1] == "runserver":
-        os.environ.setdefault("DB_PASSWORD", sys.argv[3])
-    elif sys.argv[1] == "makemigrations" or sys.argv[1] == "migrate":
-        os.environ.setdefault("DB_PASSWORD", sys.argv[2])
+    # if sys.argv[1] == "runserver":
+    #     os.environ.setdefault("DB_PASSWORD", sys.argv[3])
+    # elif sys.argv[1] == "makemigrations" or sys.argv[1] == "migrate":
+    #     os.environ.setdefault("DB_PASSWORD", sys.argv[2])
     """if run, elif makemigrations, elif migrate, elif create_experiments"""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmnt.settings")
     try:
